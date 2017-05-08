dd69a53fc776896caf9f4da267ece4d9579f8c47
Phanindra Veera <phanindrasrikar@gmail.com>
2017-05-08 18:56:35 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', './particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+
 $(document).ready(function(){
   $(".block, .proj_block").hover(function(){
     $(this).toggleClass("block-anim");
   });
 });
+
