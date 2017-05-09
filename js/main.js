697e58a74d5cbb1f57b7430ca6813d49c475bd0b
Phanindra Veera <phanindrasrikar@gmail.com>
2017-05-09 02:03:05 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', '../particlesjs-config.json', function() {
-  console.log('callback - particles.js config loaded');
-});
 
 
 $(document).ready(function(){
